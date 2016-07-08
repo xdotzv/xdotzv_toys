705fa7e0e77b47da9e6b528f85d564999a1171a5
nongeek <nongeek.zv@gmail.com>
2016-07-08 21:58:50 +0800

    calc

--- CppCalc/test.cpp
+++ CppCalc/test.cpp
@@ -2,8 +2,11 @@
 #include <stdexcept>
 #include <cstdlib>
 #include <cassert>
+#include <iostream>
+#include "AST.hpp"
 
 using std::string;
+
 
 /*
   <exp>           ::= <term> { '+' <term>}
@@ -18,100 +21,6 @@ using std::string;
 
 
 // abstract class
-class Visitor;
-using VisitorPtr = Visitor *;
-
-
-struct Exp{
-  virtual void  accept(VisitorPtr ) = 0;
-};
-using ExpPtr = Exp *;
-
-struct Binary: public Exp{
-  ExpPtr lhs, rhs;
-  Binary(ExpPtr _lhs, ExpPtr _rhs):lhs(_lhs), rhs(_rhs){}
-};
-
-struct Plus: public Binary{
-  void accept(VisitorPtr ) override;
-  Plus(ExpPtr _lhs, ExpPtr _rhs):Binary(_lhs, _rhs){}
-};
-
-struct Minus: public Binary{
-  void accept(VisitorPtr ) override;
-  Minus(ExpPtr _lhs, ExpPtr _rhs):Binary(_lhs, _rhs){}
-};
-
-struct Multiplies: public Binary{
-  void accept(VisitorPtr ) override;
-  Multiplies(ExpPtr _lhs, ExpPtr _rhs):Binary(_lhs, _rhs){}
-};
-
-struct Divides: public Binary{
-  void accept(VisitorPtr ) override;
-  Divides(ExpPtr _lhs, ExpPtr _rhs):Binary(_lhs, _rhs){}
-};
-
-struct Unary: public Exp{
-  ExpPtr p;
-  Unary(ExpPtr _p):p(_p){}
-};
-
-struct Negative: public Unary{
-  void accept(VisitorPtr ) override;
-  Negative(ExpPtr _p):Unary(_p){}
-};
-struct Positive: public Unary{
-  void accept(VisitorPtr ) override;
-  Positive(ExpPtr _p):Unary(_p){}
-};
-
-struct Number: public Exp{
-  int value;
-  void accept(VisitorPtr) override;
-  Number(int _value):value(_value){}
-};
-
-
-using PlusPtr = Plus*;
-using MinusPtr = Minus*;
-using MultipliesPtr = Multiplies*;
-using DividesPtr = Divides*;
-using PositivePtr = Positive*;
-using NegativePtr = Negative*;
-using NumberPtr = Number*;
-
-class Visitor{
-public:
-  virtual void visit(PlusPtr) = 0;
-  virtual void visit(MinusPtr) = 0;
-  virtual void visit(MultipliesPtr) = 0;
-  virtual void visit(DividesPtr) = 0;
-  virtual void visit(PositivePtr) = 0;
-  virtual void visit(NegativePtr) = 0;
-  virtual void visit(NumberPtr) = 0;
-};
-void Plus::accept(VisitorPtr visitor) {
-  visitor->visit(this);
-}
-void Minus::accept(VisitorPtr visitor) {
-  visitor->visit(this);
-}
-void Multiplies::accept(VisitorPtr visitor) {
-  visitor->visit(this);
-}
-void Divides::accept(VisitorPtr visitor) {
-  visitor->visit(this);
-}
-void Negative::accept(VisitorPtr visitor) {
-  visitor->visit(this);
-}
-void Positive::accept(VisitorPtr visitor) {
-  visitor->visit(this);
-}
-void Number::accept(VisitorPtr visitor) {
-  visitor->visit(this);
-}
 
 class Parser{
 private:
